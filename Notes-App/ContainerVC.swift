d35cc337051492fafbe56b6c579720da4e51cee5
phuquoc164 <phuquoc1604@gmail.cm>
2017-12-05 22:07:58 +0100

    Initial Commit

---
+++ Notes-App/ContainerVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes-App
+//
+//  Created by Dinh Phu Quoc on 05/12/2017.
+//  Copyright © 2017 if26. All rights reserved.
+//
+
+import UIKit
+
+class ContainerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
